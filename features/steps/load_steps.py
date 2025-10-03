ee2300e812e19e0bb055e6cc2283d19ea8d83f44
knr83 <165853377+knr83@users.noreply.github.com>
2025-10-03 16:30:48 +0200

    Update load_steps.py with corrected content

--- features/steps/load_steps.py
+++ features/steps/load_steps.py
@@ -30,6 +30,7 @@ HTTP_200_OK = 200
 HTTP_201_CREATED = 201
 HTTP_204_NO_CONTENT = 204
 
+
 @given('the following products')
 def step_impl(context):
     """ Delete all Products and load new ones """
@@ -38,10 +39,10 @@ def step_impl(context):
     #
     rest_endpoint = f"{context.base_url}/products"
     context.resp = requests.get(rest_endpoint)
-    assert(context.resp.status_code == HTTP_200_OK)
+    assert context.resp.status_code == HTTP_200_OK
     for product in context.resp.json():
         context.resp = requests.delete(f"{rest_endpoint}/{product['id']}")
-        assert(context.resp.status_code == HTTP_204_NO_CONTENT)
+        assert context.resp.status_code == HTTP_204_NO_CONTENT
 
     #
     # load the database with new products
